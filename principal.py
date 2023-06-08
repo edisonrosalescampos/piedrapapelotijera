b3665e42a82d1e44caf91675a270f46ec5e0762b
EDISON ROSALES <edisonrosalescampos@gmail.com>
2023-06-08 12:38:45 -0400

    versión 1.0

---
+++ principal.py
@@ -0,0 +1,38 @@
+import random
+
+def jugar():
+  opciones    = ["piedra", "papel", "tijera"]
+  computadora = opciones[random.randint(0, 2)] 
+
+  print("BIENVENIDO AL JUEGO DE PIEDRA, PAPEL Y TIJERA")
+
+  usuario = input("Escriba piedra, papel o tijera: ")
+
+  print(f"Tú: {usuario} vs PC: {computadora}")
+
+  # Empate
+  if usuario == computadora:
+    print("¡Empate!")
+
+  # Piedra
+  if usuario == opciones[0]:
+    if computadora == opciones[1]:
+      print("¡Perdiste!")
+    if computadora == opciones[2]:
+      print("¡Ganaste!")
+
+  # Papel
+  if usuario == opciones[1]:
+    if computadora == opciones[0]:
+      print("¡Ganaste!") 
+    if computadora == opciones[2]:
+      print("¡Perdiste!")  
+
+  # Tijera
+  if usuario == opciones[2]:
+    if computadora == opciones[0]:
+      print("¡Perdiste!")
+    if computadora == opciones[1]:
+      print("¡Ganaste!")
+
+jugar()
