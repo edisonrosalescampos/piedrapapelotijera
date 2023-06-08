536766cb1150edb638c86f2c01f8d0b434fab418
EDISON ROSALES <edisonrosalescampos@gmail.com>
2023-06-08 12:56:33 -0400

    Versión 1.0

--- principal.py
+++ principal.py
@@ -1,38 +1,44 @@
 import random
 
 def jugar():
-  opciones    = ["piedra", "papel", "tijera"]
-  computadora = opciones[random.randint(0, 2)] 
-
   print("BIENVENIDO AL JUEGO DE PIEDRA, PAPEL Y TIJERA\n")
 
-  usuario = input("Escriba piedra, papel o tijera: ")
+  continuar = True
+  while continuar:
+    opciones    = ["piedra", "papel", "tijera"]
+    computadora = opciones[random.randint(0, 2)] 
+    usuario     = input("Escriba piedra, papel o tijera: ")
 
-  print(f"Tú: {usuario} vs PC: {computadora}")
+    print(f"Tú: {usuario} vs PC: {computadora}")
 
-  # Empate
-  if usuario == computadora:
-    print("¡Empate!")
+    # Empate
+    if usuario == computadora:
+      print("¡Empate!")
 
-  # Piedra
-  if usuario == opciones[0]:
-    if computadora == opciones[1]:
-      print("¡Perdiste!")
-    if computadora == opciones[2]:
-      print("¡Ganaste!")
+    # Piedra
+    if usuario == opciones[0]:
+      if computadora == opciones[1]:
+        print("¡Perdiste!")
+      if computadora == opciones[2]:
+        print("¡Ganaste!")
 
-  # Papel
-  if usuario == opciones[1]:
-    if computadora == opciones[0]:
-      print("¡Ganaste!") 
-    if computadora == opciones[2]:
-      print("¡Perdiste!")  
+    # Papel
+    if usuario == opciones[1]:
+      if computadora == opciones[0]:
+        print("¡Ganaste!") 
+      if computadora == opciones[2]:
+        print("¡Perdiste!")  
 
-  # Tijera
-  if usuario == opciones[2]:
-    if computadora == opciones[0]:
-      print("¡Perdiste!")
-    if computadora == opciones[1]:
-      print("¡Ganaste!")
+    # Tijera
+    if usuario == opciones[2]:
+      if computadora == opciones[0]:
+        print("¡Perdiste!")
+      if computadora == opciones[1]:
+        print("¡Ganaste!")
+
+    print()
+
+    reiniciar = input("¿Quiere seguir jugando? (s/n): ")
+    continuar = reiniciar.lower() == "s"
 
 jugar()
